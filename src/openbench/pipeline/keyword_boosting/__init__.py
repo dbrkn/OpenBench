77b3d8009c3977aa93018f2836f5c39083f2f8a3
dberkin1 <berkin@argmax.com>
2025-09-15 17:35:08 +0300

    Add Nemo Context biasing pipeline to boosting

--- src/openbench/pipeline/keyword_boosting/__init__.py
+++ src/openbench/pipeline/keyword_boosting/__init__.py
@@ -2,4 +2,5 @@
 # Copyright (C) 2025 Argmax, Inc. All Rights Reserved.
 
 from .deepgram import DeepgramBoostingPipeline, DeepgramBoostingPipelineConfig
+from .nemo import NeMoBoostingPipeline, NeMoBoostingPipelineConfig
 from .openai import OpenAIBoostingPipeline, OpenAIBoostingPipelineConfig
